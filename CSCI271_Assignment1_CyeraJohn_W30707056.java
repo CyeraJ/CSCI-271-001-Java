fcfd38fd4511e88d86cd9424e07f3c4fff755923
Cyera John <johnc2@winthrop.edu>
2026-01-22 12:45:35 -0500

    First commit

---
+++ CSCI271_Assignment1_CyeraJohn_W30707056.java
@@ -0,0 +1,24 @@
+/*************************************************************************
+* Assignment 1 for CSCI 271-001 Spring 2026
+*
+* Author: Cyera John
+* OS: Ubuntu Debian 24.04.3
+* Compiler: javac 21.0.9
+* Date: January 21, 2026
+*
+* Purpose
+* My program reads a list of scores from an input file, counts how many
+* scores there are in the list, and computes their sum and average.
+*
+*************************************************************************/
+import java.util.Scanner;
+
+public class CSCI271_Assignment1_CyeraJohn_W30707056 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int x = input.nextInt();
+		String sentence = input.nextLine();
+		input.close(); // Make sure to always
+			// close your input objects
+	}
+}
